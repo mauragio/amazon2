f90e675554b3e5f385d769925e2e3391e97ac54c
mauragio <giovannimauramati@gmail.com>
2015-07-21 15:23:51 +0200

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,24 +1,18 @@
 <?
 // This is a template for a PHP scraper on morph.io (https://morph.io)
 // including some code snippets below that you should find helpful
-
-// require 'scraperwiki.php';
-// require 'scraperwiki/simple_html_dom.php';
-//
-// // Read in a page
-// $html = scraperwiki::scrape("http://foo.com");
-//
-// // Find something on the page using css selectors
-// $dom = new simple_html_dom();
-// $dom->load($html);
-// print_r($dom->find("table.list"));
-//
-// // Write out to the sqlite database using scraperwiki library
-// scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
-//
-// // An arbitrary query against the database
-// scraperwiki::select("* from data where 'name'='peter'")
-
+require 'scraperwiki.php';
+require 'scraperwiki/simple_html_dom.php';
+// Read in a page
+$html = scraperwiki::scrape("http://www.amazon.it/gp/product/B00PHCLIRA");
+// Find something on the page using css selectors
+$dom = new simple_html_dom();
+$dom->load($html);
+print_r($dom->find("td[id=priceblock_ourprice_lbl]"));
+// Write out to the sqlite database using scraperwiki library
+//scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
+// An arbitrary query against the database
+//scraperwiki::select("* from data where 'name'='peter'")
 // You don't have to do things with the ScraperWiki library.
 // You can use whatever libraries you want: https://morph.io/documentation/php
 // All that matters is that your final data is written to an SQLite database
